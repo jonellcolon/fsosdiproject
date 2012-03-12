bc0db8acfd2aa227a696c57c92ccd9d4da500e7e
Jonell Colon <jonellcolon@Jonells-MacBook-Pro.local>
2012-03-12 01:41:07 -0400

    Added object coding on JavaScript.

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,37 @@
-alert("JavaScript works!");
+alert("SDI Project 3");
+
+var car1 = { 
+	make: "Pontiac", 
+	model: "Grand Prix", 
+	year: 1997 
+	
+};
+
+var car2 = { 
+	make: "Honda", 
+	model: "Civic", 
+	year: 2002
+	
+};
+
+function carDetails() {
+	// display car details here
+	console.log(this.make + " is a " + this.model + " from " + this.year);
+
+}
+
+car1.logDetails = carDetails;
+car2.logDetails = carDetails;
+
+car1.logDetails();
+car2.logDetails();
+
+for (var specs in car1) {
+	console.log("specs: " + specs + ", info: ", car1[specs]);
+	
+};
+
+for (var specs in car2) {
+	console.log("specs: " + specs + ", info: ", car2[specs]);
+	
+};
