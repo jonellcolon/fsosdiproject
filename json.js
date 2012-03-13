eca084df849557e1888184f65fb6b20b72bc39cf
Jonell Colon <jonellcolon@Jonells-MacBook-Pro.local>
2012-03-13 00:03:01 -0400

    Added JSON file to script.

---
+++ json.js
@@ -0,0 +1,50 @@
+// JSON Data File
+
+var json = {
+	"cars": {
+		"001": {
+			"maker": "Pontiac",
+			"year": 1997,
+			"model": "Grand Prix"
+			},
+		"002": {
+			"maker": "Honda",
+			"year": 2002,
+			"model": "Civic"
+			},
+		"003": {
+			"maker": "Toyota",
+			"year": 2000,
+			"model": "Accord"
+			}
+	}
+};
+
+/* for (var key in json.cars) {
+	var car = json.cars[key]
+*/
+
+var json2 = {
+	"cars": [
+		{
+			"uuid": "001",
+			"maker": "Pontiac",
+			"year": 1997,
+			"model": "Grand Prix"
+		},
+		{
+			"uuid": "002", 
+			"maker": "Honda",
+			"year": 2002,
+			"model": "Civic"
+		},
+		{
+			"uuid": "003",
+			"maker": "Toyota",
+			"year": 2000,
+			"model": "Accord"
+		}
+	]
+};
+	
+		
--- rename-me.js
+++ rename-me.js
@@ -1,16 +1,29 @@
-alert("SDI Project 3");
+var me = "I",
+	buy = true,
+	money = "$2,000",
+	lookAtVehicles = "Car Model"
+	
+if (buy === true) {
+	console.log("I am looking into cars to buy.")
+} else { 
+	console.log("Keep saving money.")
+};
+
+/*alert("SDI Project 3");
 
 var car1 = { 
 	make: "Pontiac", 
 	model: "Grand Prix", 
-	year: 1997 
+	year: 1997,
+	looks:"Used"
 	
 };
 
 var car2 = { 
 	make: "Honda", 
 	model: "Civic", 
-	year: 2002
+	year: 2002,
+	looks: "New"
 	
 };
 
@@ -22,6 +35,7 @@ function carDetails() {
 
 car1.logDetails = carDetails;
 car2.logDetails = carDetails;
+
 
 car1.logDetails();
 car2.logDetails();
@@ -35,3 +49,21 @@ for (var specs in car2) {
 	console.log("specs: " + specs + ", info: ", car2[specs]);
 	
 };
+*/
+
+var handleData = function (json) {
+	for (var i = 0; i < json.cars.length; i++){
+		var car = json.cars[i];
+		console.log("Car ID: " + car.uuid + ", Maker: " + car.maker + ", Model: " + car.model + ", Year: " + car.year);
+	};
+};
+
+handleData(json2);
+
+/*
+var jsonstring = JSON.stringify(json);
+	console.log(jsonstring);
+
+var receivedjson = JSON.parse(jsonstring);
+console.log(receivedjson);
+*/
