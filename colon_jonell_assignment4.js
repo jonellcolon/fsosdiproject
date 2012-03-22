01f2551b7284b109b0ca5fc4f665caef1ec9d851
Jonell Colon <jonellcolon@Jonells-MacBook-Pro.local>
2012-03-22 02:26:05 -0400

    Finished coding and fixing a bug in the return for the library.

--- colon_jonell_assignment4.js
+++ colon_jonell_assignment4.js
@@ -5,7 +5,7 @@
 
 alert("SDI Project 4, Week 4");
 
-var jonellLib = function () {}
+var jonellLib = function (){
 
 // E-Mail Validation
 // http://www.authorcode.com/how-to-validate-email-address-in-javascript/
@@ -27,7 +27,7 @@ var emailValidation = function (emailValidate) {
 // Phone Number Validation
 // http://www.zparacha.com/phone_number_regex/#.T2f9RXjVlAc
 
-var phoneValidation = function (validatePhoneNumber){  
+var phoneValidation = function (validatePhoneNumber) {  
 					/* The regex means that number may start with an optional "(" then followed 
 					by 3 numbers then an optional ")" or "-" followed by 3 more numbers, 
 					another optional "-" and end with 4 numbers. */
@@ -53,32 +53,34 @@ var titleCase = function (title) {
         function cnvrt() {
             return arguments[0].toUpperCase();
         }
-    }
+};
 
 
 // URL Validation (HTTP or HTTPS)
 // http://msdn.microsoft.com/en-us/library/ff650303.aspx
 
-var urlValidation = function (url);
+var urlValidation = function (url) {
 							/* The regexp is giving us the option of looking for validation of a url
 							"ftp, http or https". Also looks for url starting with numbers,
 							 lower case and upper case words. Continue to look for "." and then
 							 seeks numbers, lower case and upper case words finish the validation
 							 after seeing the "." and checking once more for numbers, lower case 
 							 and upper case words. */
-	var regexp = ^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$
-	if (regexp.test(urlValidation)) {
+	var regexp = /^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$/;
+	if (regexp.test(url)) {
     	console.log("URL is correct");
 } else {
     	console.log("URL is not correct");
-}
+
+};
 
 // Format number to specific number of decimal spaces, as for money
 // http://www.mredkj.com/javascript/nfbasic2.html
 
-var decimalPrecision = function (num)
-	var amount = num.toFixed(2); // Will give 2 decimal points because specified "(2)".
-	console.log(amount)
+var value = 23;
+	var amount = value.toFixed(2); // Will give 2 decimal points because specified "(2)".
+	console.log(amount);
+
 
 // Total value of just numbers in an Array
 
@@ -87,20 +89,40 @@ var totalArray = ['jonell',31,'Rose',29,'Adassa',3,'Eli',5];
 	for (var i=0; i < totalArray.length; i++) {
   	if (!isNaN(parseInt(totalArray[i]))) { total += totalArray[i]; }
 }
-console.log(total);
+	console.log(total);
 
-
-
+};
 
 return {
+
 	"emailValidation" : emailValidation,
 	"phoneValidation" : phoneValidation,
 	"titleCase" : titleCase,
 	"urlValidation" : urlValidation,
-	"decimalPrecision" : decimalPrecision,
+	"value" : value,
 	"totalArray" : totalArray
 	
 	};
 };
 
 var lib = jonellLib();
+
+var emailValidate = ["jonellcolon@fullsail.edu"];
+var validatePhoneNumber = [407-437-7526];
+var title = ["scalable data infrastructure"];
+var url = ["http://www.fullsail.edu"];
+var value = [];
+var totalArray = [];
+
+/*console.log(lib.emailValidation("jonellcolon@fullsail.edu"));
+console.log(lib.phoneValidation("(407) 437-7526"));
+console.log(lib.titleCase("scalable data infrastructure"));
+console.log(lib.urlValidation("http://www.fullsail.edu"));
+// console.log(lib.value());
+// console.log(lib.totalArray());
+*/
+
+lib.emailValidation("jonellcolon@fullsail.edu");
+lib.phoneValidation("(407) 437-7526");
+console.log(lib.titleCase("scalable data infrastructure"));
+lib.urlValidation("http://www.fullsail.edu");
